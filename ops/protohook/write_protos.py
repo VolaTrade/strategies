c0f34a28b673544f7396352e76e1a78384467c81
epociask <epociask@luc.id>
2021-03-29 22:02:32 -0700

    [PROTO_AUTOMATION] automation works now

--- ops/protohook/write_protos.py
+++ ops/protohook/write_protos.py
@@ -1,18 +1,11 @@
-from os import listdir, system, chdir
-import time
+from os import system, listdir
 
 if __name__ == '__main__':
-    
-    t = time.time()
-    branch_name = hash(t)
 
     for proto_file in listdir("proto_files"):
         file = str(proto_file)
         attr_name = file.replace(".proto", "")
         system(f"cp proto_files/{proto_file} protobufs/strategies/{attr_name}/{file}")
     
-    chdir("protobufs")
-    system(f"git checkout -b {branch_name}")
-    system("git add .")
-    system("""git commit -m "[{branch_name}] Automated files" """)
-    system(f"git push --set-upstream origin {branch_name}")
+
+
